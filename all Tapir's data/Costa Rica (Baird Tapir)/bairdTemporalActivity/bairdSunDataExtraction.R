3848cbc50620a869827eb681251d4572020cbf51
canderson318 <christianjbanderson@gmail.com>
2023-06-05 16:30:40 -0700

    edited baird sun data extraction script
    filtered data for independent detections and got sun data for that

--- all Tapir's data/Costa Rica (Baird Tapir)/bairdTemporalActivity/bairdSunDataExtraction.R
+++ all Tapir's data/Costa Rica (Baird Tapir)/bairdTemporalActivity/bairdSunDataExtraction.R
@@ -25,16 +25,17 @@ require("lubridate")
 require("astroFns")
 
 
-setwd("C:/Users/chris/Documents/Research")
+setwd("C:/Users/chris/Documents/Research/Tapir Reasearch/Code and Data")
 
 #dataframe needs"lat", "lon", "date" (Y-M-D), and "hour" (H:M) column headers
 #the function wants a 'species' column to work
-lowland.raw<- read.csv("all Tapir's data/Amazon (Lowland Tapir)/AM_Records-Dataset01_CEP-GBR.csv", comment.char = '#')
-baird.raw<- read.csv("all Tapir's data/Costa Rica (Baird_s Tapir)/Master(5.31.22).csv")
+baird.raw<- read.csv("all Tapir's data/Costa Rica (Baird Tapir)/Master(5.31.22).csv")
 #remove rows with nas
-lowland.raw<- na.omit(lowland.raw)
+
 baird.raw<- na.omit(baird.raw)
 
+#grabs rows for where independent is "Yes" or "YES"
+baird.raw<- baird.raw[(baird.raw$Independent == "Yes") | (baird.raw$Independent == "YES"),]
 #pull out neccessary columns for each df
 baird<-data.frame(
   "date" = baird.raw$Date,
@@ -44,17 +45,12 @@ baird<-data.frame(
   "species"= baird.raw$Species
 )
 
-lowland<-data.frame(
-  "date" = lowland.raw$date,
-  "lat" = lowland.raw$lat,
-  "lon" = lowland.raw$long,
-  "hour" = lowland.raw$hour,
-  "species" = lowland.raw$species
-)
 
-lowland<- lowland[lowland$species == "anta" , ]
+
+
 baird<- baird[baird$species == "Tapirus bairdii" , ]
 
+{
 #check if df headers match requirments (date, species, hour, lat, lon)
   # checkHead<- function(df){
   #   headers<- c('hour', 'lat', 'lon', 'date', 'species')
@@ -76,7 +72,7 @@ baird<- baird[baird$species == "Tapirus bairdii" , ]
   # new.names<- c('date', 'hour', 'lat', 'lon', 'species')
   # col.nums<- match(old.names, colnames(baird)) #column numbers to update
   # colnames(baird)[col.nums] <- new.names #change column names to new column names
-
+}
 #function creates time and date objects with given date and time formats and converts time stamp to radian time
 cleanTimeDate<- function(df, dateFormat, timeFormat){
   #change date and time formats
@@ -88,7 +84,6 @@ cleanTimeDate<- function(df, dateFormat, timeFormat){
   return(df)
 }
 
-lowland<- cleanTimeDate(lowland, dateFormat = '%d/%m/%Y', timeFormat= "%H:%M")
 baird<- cleanTimeDate(baird, dateFormat = "%m/%d/%Y", timeFormat = '%H:%M:%S')
 
 
@@ -151,13 +146,7 @@ solarhour <- function(dat, tzone) {
   return(solar)
 }
 
-sunData.lowland <- data.frame(
-  "date" = lowland$date,
-  "lat" = lowland$lat,
-  "lon" = lowland$lon,
-  "hour" = lowland$hour,
-  "species" = lowland$species
-)
+
 sunData.baird<- data.frame(
   "date" = baird$date,
   "lat" = baird$lat,
@@ -166,8 +155,9 @@ sunData.baird<- data.frame(
   "species" = baird$species
 )
 
-sunData.lowland["solar"] <- solarhour(sunData.lowland, tzone=  'America/Porto_Velho')
+
 sunData.baird["solar"] <- solarhour(sunData.baird, tzone= 'America/Costa_Rica')
+
 # Save as CSV of each df
-write.csv(sunData.lowland, "lowlandSunData.csv", row.names = FALSE)
-write.csv(sunData.baird, "bairdSunData.csv", row.names = FALSE)
+path<- "C:/Users/chris/Documents/Research/Tapir Research/Code and Data/all Tapir's data/Costa Rica (Baird Tapir)/bairdTemporalActivity/"
+write.csv(sunData.baird, paste(path, "bairdSunDataIndependent.csv", sep=''), row.names = FALSE)
