9277305c1550bda6b567cfa0c757b04573b45e80
canderson318 <christianjbanderson@gmail.com>
2023-07-13 15:37:41 -0700

    Update makeMatrix.R

--- all Tapir's data/Peru (Mountain Tapir)/makeMatrix.R
+++ all Tapir's data/Peru (Mountain Tapir)/makeMatrix.R
@@ -1,39 +1,27 @@
 # Example dataset
-dataset <- data.frame(
-  Sensor = c("Sensor1", "Sensor1", "Sensor2", "Sensor3", "Sensor2"),
-  Date = c("2023-01-01", "2023-01-01", "2023-01-02", "2023-01-03", "2023-01-02")
+data <- data.frame(
+  Sensor = c("Sensor1", "Sensor1", "Sensor2", "Sensor3", "Sensor2", "Sensor1", "Sensor3", "Sensor3"),
+  Date = as.Date(c("2023-01-01", "2023-01-02", "2023-01-02", "2023-01-03", "2023-01-04", "2023-01-04", "2023-01-05", "2023-01-05"))
 )
 
-# Define study start and end dates
-start_date <- as.Date("2023-01-01")
-end_date <- as.Date("2023-04-30")
+# Determine the start and end dates of the study period
+start_date <- min(data$Date)
+end_date <- max(data$Date)
 
-# Create sequence of dates
-dates <- seq(start_date, end_date, by = "day")
+# Create a sequence of dates from start to end
+date_sequence <- seq(start_date, end_date, by = "day")
 
-# Create empty matrix
-sensor_matrix <- matrix(0, nrow = length(unique(dataset$Sensor)), ncol = length(dates),
-                        dimnames = list(unique(dataset$Sensor), dates))
-rnames<- rownames(dataset)
-cnames<- colnames(dataset)
+# Create the binary matrix
+trigger_matrix <- matrix(0, nrow = length(unique(data$Sensor)), ncol = length(date_sequence),
+                         dimnames = list(unique(data$Sensor), date_sequence))
 
-# Update matrix with triggered values
-sensor_matrix[dataset$Sensor, dataset$Date] <- 1
-
-
-
-
-sensor_matrix
-
-#((((((((((((((((((((((((((((((((((((((((()))))))))))))))))))))))))))))))))))))))))
-# Create an empty matrix with rows for each sensor and columns for each day
-sensor_matrix <- matrix(0, nrow = length(unique(dataset$Sensor)), ncol = length(unique(dataset$Date)))
-
-# Get the indices of sensors and dates in the matrix
-sensor_indices <- match(dataset$Sensor, unique(dataset$Sensor))
-date_indices <- match(dataset$Date, unique(dataset$Date))
-
-# Assign 1 to the corresponding cells in the matrix
-sensor_matrix[cbind(sensor_indices, date_indices)] <- 1
+# Set the values in the matrix to 1 for triggered days
+for (i in seq_along(data$Sensor)) {
+  sensor <- data$Sensor[i]
+  date <- data$Date[i]
+  rindex<- which(rownames(trigger_matrix) == sensor)
+  cindex<- which(date_sequence == date)
+  trigger_matrix[rindex, cindex] <- 1
+}
 
 
